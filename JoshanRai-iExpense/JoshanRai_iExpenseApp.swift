bf729a94d3dac810a786a86c7e175e63e3232981
Joshan Rai <pradheon@gmail.com>
2022-03-07 14:41:34 -0800

    Initial Commit

---
+++ JoshanRai-iExpense/JoshanRai_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_iExpenseApp.swift
+//  JoshanRai-iExpense
+//
+//  Created by Joshan Rai on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
