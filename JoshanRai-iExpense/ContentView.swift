cdd01f1879fb5b2899dfd3f1a93c1c1004609f94
Joshan Rai <pradheon@gmail.com>
2022-03-09 15:20:25 -0800

    v0.5 - Storing user settings with UserDefaults

--- JoshanRai-iExpense/ContentView.swift
+++ JoshanRai-iExpense/ContentView.swift
@@ -9,43 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+    //@State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @AppStorage ("tapCount") private var tapCount = 0
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(numbers, id: \.self) {
-                    Text("Row \($0)")
-                }
-                .onDelete(perform: removeRows)
-            }
-            
-            Button("Add Number") {
-                numbers.append(currentNumber)
-                currentNumber += 1
-            }
-        }
-        .toolbar {
-            EditButton()
-        }
-    }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-}
-
-struct SecondView: View {
-    @Environment (\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Text("Hello, \(name)!")
-        
-        Button("Dismiss") {
-            dismiss()
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+            //UserDefaults.standard.set(self.tapCount, forKey: "Tap")
         }
     }
 }
