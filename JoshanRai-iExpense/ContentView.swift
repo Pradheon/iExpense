b52a091095c678a92290a971d8dd44885a219b19
Joshan Rai <pradheon@gmail.com>
2022-03-09 14:55:59 -0800

    v0.2  Why @State only works with structs  Sharing SwiftUI stat with @StateObject

--- JoshanRai-iExpense/ContentView.swift
+++ JoshanRai-iExpense/ContentView.swift
@@ -4,13 +4,30 @@
 //
 //  Created by Joshan Rai on 3/7/22.
 //
+//  Made with help from: https://www.hackingwithswift.com/100/swiftui/36
 
 import SwiftUI
 
+//   Why @State only works with structs
+//   Sharing SwiftUI stat with @StateObject
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    //   Why @State only works with structs
+    //   Sharing SwiftUI stat with @StateObject
+    @StateObject var user = User()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            //   Why @State only works with structs
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name: ", text: $user.firstName)
+            TextField("Last name: ", text: $user.lastName)
+        }
     }
 }
 
